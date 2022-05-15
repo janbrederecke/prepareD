6dbe153a568a904dd7f16f85ae555460c6674234
janbrederecke <j.brederecke@uke.de>
2022-05-15 14:07:32 +0200

    Improved the .find_continouus function to accept an input vector to return all numeric variables not specified in the input vector

--- R/find_continouus.R
+++ R/find_continouus.R
@@ -4,16 +4,37 @@
 #' unique values
 #'
 #' @param .data A data.frame or tibble
+#' @param .bin_var A character vector containing all binary variables
+#' (if known). This results in the output of all numeric variables not in this
+#' string.
 #' 
 #' @return A character vector with the numeric variables
 #' @examples -
 #' @export
 
 find_continouus <- function(.data
+                            , .bin_var = NULL
 ){
   
   # Get all numeric variables
   numeric_variables <- names(.data[, unlist(lapply(.data, is.numeric))])
+  
+  # Create output in case a .bin_var vector was specified
+  if (!is.null(.bin_var)) {
+    
+    # Check if .bin_var is not a character vector 
+    if (class(.bin_var) != "character") {
+      stop(".bin_var needs to be a character vector!")
+    }
+    
+    # Keep all numeric variables not in .bin_var
+    cont_var <- setdiff(numeric_variables, .bin_var)
+    
+    # Return output vector
+    cont_var
+    
+  # Create output for unspecified .bin_var (return numerics mit 'unique >= 3')  
+  } else {
   
   # Count unique values but NA in variables to be tested
   to_test <- unlist(lapply(.data[, numeric_variables], function(x)
@@ -26,4 +47,5 @@ find_continouus <- function(.data
   
   # Return names of binary variables as output
   cont_var
+  }
 }
