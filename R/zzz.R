baedc38bd6fcc5adf97a971315fa751bda0a0c5a
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-03 23:10:14 +0200

    Added a startup message.

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+# Package startup message
+.onAttach <- function(libname
+                      ,pkgname
+){
+  
+  msg <- paste0(
+    "\n",
+    "== Thank you for using prepareD =========================================",
+    "\nIf you encounter any bugs feel free to file an issue at:",
+    "\nhttps://github.com/janbrederecke/prepareD",
+    "\n",
+    "\nYou might want to check my other packages at:",
+    "\nhttps://github.com/janbrederecke"
+  )
+  
+  packageStartupMessage(msg)
+}
