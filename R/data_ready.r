a9433d037ea46ee60032aa1a6dad72abe69465cf
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-10 15:55:39 +0200

    Trying the git tool in VSCode

--- R/data_ready.r
+++ R/data_ready.r
@@ -8,11 +8,12 @@
 #' @examples -
 #' @export
 
-    data_ready <- function(.data){
-        
-        # Check if data is a tibble and if not, turn it into one (automatically checks for valid names)
-        if (class(.data)[1] == "data.frame") {
-        .data <- tibble::as_tibble(.data)
-        } else if (!class(.data)[1] %in% "tbl_df") {
-        stop("Input must be a data.frame or tibble.")
-        }
+data_ready <- function(.data
+){    
+    # Check if data is a tibble and if not, turn it into one (automatically checks for valid names)
+    if (class(.data)[1] == "data.frame") {
+    .data <- tibble::as_tibble(.data)
+    } else if (!class(.data)[1] %in% "tbl_df") {
+    stop("Input must be a data.frame or tibble.")
+    }
+}
