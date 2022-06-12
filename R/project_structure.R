5aa1ca16af12890e4835b47f236388835c602047
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-12 15:30:49 +0200

    correcting the project_structure function

--- R/project_structure.R
+++ R/project_structure.R
@@ -1,20 +1,13 @@
-project_structure <- function(path = "") {
-    if (path == "") {
-        path <- getwd()
-    }
-
-    # Create the project directory structure
-        all_files <- list.files(system.file("inst/templates/basic",
-                                            package = "prepareD"),
-                                full.names = TRUE)
-
-        file.copy(
-            from = all_files,
-            to = path,
-            overwrite = TRUE,
+project_structure <- function(path = "") 
+{
+  if (path == "") {
+    path <- getwd()
+  }
+  all_files <- list.files(system.file("templates/basic", 
+                                      package = "prepareD"), full.names = TRUE)
+  file.copy(from = all_files, to = path, overwrite = TRUE, 
             recursive = TRUE)
-
-        dir.create(paste0(path, "data"))
-        dir.create(paste0(path, "data/raw"))
-        dir.create(paste0(path, "data/clean"))
+  dir.create(paste0(path, "/data"))
+  dir.create(paste0(path, "/data/raw"))
+  dir.create(paste0(path, "/data/clean"))
 }
