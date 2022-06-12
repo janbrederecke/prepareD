ce885ead9c0d80732f56bc299c25a7d5c1c3a220
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-12 12:02:49 +0200

    Working on the function

--- R/project_structure.R
+++ R/project_structure.R
@@ -1,14 +1,20 @@
 project_template_init <- function(path = "") {
     if (path == "") {
         path <- getwd()
-    }   
+    }
 
-    
-        all_files <- list.files(system.file("inst/templates/basic", package = "prepareD"), full.names = TRUE)
+    # Create the project directory structure
+        all_files <- list.files(system.file("inst/templates/basic",
+                                            package = "prepareD"),
+                                full.names = TRUE)
 
-        file.copy(  
+        file.copy(
             from = all_files,
             to = path,
             overwrite = TRUE,
             recursive = TRUE)
+
+        dir.create(paste0(path, "data"))
+        dir.create(paste0(path, "data/raw"))
+        dir.create(paste0(path, "data/clean"))
 }
