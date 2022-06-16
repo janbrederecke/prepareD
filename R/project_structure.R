6129c58953c382ba22827f2385b55d99422fa133
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-06-16 10:27:04 +0200

    project_structure now able to call diff templates

--- R/project_structure.R
+++ R/project_structure.R
@@ -3,13 +3,16 @@
 #' @description Creates a project structure in the current working directory.
 #'
 #' @param .path A character vector containing the path to the project.
-
+#' @param .template Defaults to "basic". Character vector that can be used to
+#' choose user-defined templates that have been saved to
+#' "system.file/templates".
 #'
 #' @return A character vector with the numeric variables.
 #' @examples -
 #' @export
 
 project_structure <- function(.path = ""
+                              , .template = "basic"
 ){
 
   # Check the path
@@ -18,7 +21,7 @@ project_structure <- function(.path = ""
   }
 
   # Copy the template files to the current working directory
-  all_files <- list.files(system.file("templates/basic",
+  all_files <- list.files(system.file(paste0("templates/", .template),
                                       package = "prepareD"),
                           full.names = TRUE)
   file.copy(from = all_files, to = .path, overwrite = TRUE,
